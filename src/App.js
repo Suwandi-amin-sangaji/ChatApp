5f11d0577af169f2773ea4216891575e34d7c1db
suwandi amin sangaji <suwandiaminsangaji@gamil.com>
2022-01-07 21:51:52 +0900

    chat app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = 'dd38e632-4942-4a42-80b4-98b1329b0d84';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName="suwandiaminsangaji"
+      userSecret="wandy123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+export default App;
+
+
+// dd38e632-4942-4a42-80b4-98b1329b0d84
